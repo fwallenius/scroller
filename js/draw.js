7151239e9906aec646f04b0668409600f59ea0e1
Fredrik Wallenius <fredrik.wallenius@accedo.tv>
2013-04-11 08:58:35 +0200

    Removing test code

--- js/draw.js
+++ js/draw.js
@@ -35,31 +35,8 @@ APP.Draw = function(canvasId, width, height) {
 
         addEntity: function(entity) {
             entities.push(entity);
-        },
-
-        test: function() {
-            console.log("Loading");
-            var img = new Image();
-            img.onload = function() {
-                console.log("Loaded");
-                context.drawImage(img, 0, 0, 500, 640);
-            }
-            img.src = "images/bg1.png";
-
-            var img2 = new Image();
-            img2.onload = function() {
-                console.log("Loaded");
-                context.drawImage(img2, 500, 0, 500, 640);
-            }
-            img2.src = "images/bg2.png";
-
-            var img3 = new Image();
-            img3.onload = function() {
-                console.log("Loaded");
-                context.drawImage(img2, 1000, 0, 500, 640);
-            }
-            img3.src = "images/bg3.png";
         }
+        
     }
 
 }
